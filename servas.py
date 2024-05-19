34be26fa2ddb8f140ca12c937a12be41b2abd1c7
Minbu5 <arkk250@gmail.com>
2024-05-19 19:02:24 +0300

    General look of recipe in flask app where recipies could be stored and published

--- servas.py
+++ servas.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# import render tempalte to render static files which shall be in templates folder
-# for other statics as images or video files shall be in static folder also might be in subfolders with path designated.
-# Note CSS file also has to be in statics
-
 
 @app.route('/')
 def home():
