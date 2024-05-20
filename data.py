a30710111ff7986a7c44fe1facb6b5740681f2f5
Minbu5 <arkk250@gmail.com>
2024-05-20 09:36:58 +0300

    Data structure reformat for npoint API and saive in data.py

---
+++ data.py
@@ -0,0 +1,230 @@
+[
+    {
+        "id": 1,
+        "category": "soups",
+        "name": "Bread, egg and garlic soup",
+        "description": "This is comfort food at its finest. It's a warm, simple and easy dish that can be enjoyed for breakfast, lunch or dinner.",
+        "ingridients": [
+            "1 tablespoon olive oil",
+            "4 garlic cloves, pressed (or finely chopped)",
+            "1/8 teaspoon hot pepper flakes",
+            "2 slices whole wheat bread, darkley toasted and cut into 1 inch cubes",
+            "2 cups low sodium chicken broth",
+            "1 tablespoon fresh parsley, chopped",
+            "2 eggs",
+            "salt and pepper grated parmesan cheese or romano cheese at your taste"
+        ],
+        "preparation": "Toast and cube the bread. Chop the parsley. Heat the olive oil in a heavy bottomed sauce pot over medium heat. Add the garlic and pepper flakes and cook for 2 minutes. Add the bread cubes and stir until the bread absorbs the oil (about 30 seconds). Add the stock and parsley. Bring to a low simmer and cook for 6 minutes. Crack the eggs into seperate small ramekins. Sprinkle a little salt and pepper on top of each egg and gently add them to the soup. Cover the pot and poach the eggs for 3 minutes (or to your liking). To serve, place one poached egg in a bowl and cover with 1/2 of the bread and broth - keep in mind the egg will continue to cook once in the bowl. Sprinkle each bowl with a little grated parmesan cheese and ground pepper. Enjoy!"
+    },
+    {
+        "id": 2,
+        "category": "soups",
+        "name": "Lithuanian Šaltibarščiai (Cold Beet Soup)",
+        "description": "This Lithuanian family recipe for šaltibarščiai is from the old country. It makes an awesome summer soup! Serve with warm Yukon Gold potatoes.",
+        "ingridients": [
+            "4 large eggs",
+            "1 quart buttermilk",
+            "1 pound beets, peeled and shredded",
+            "1 large English cucumber - peeled, quartered, and sliced",
+            "1 bunch fresh dill, minced",
+            " 1/4 cup minced chives"
+        ],
+        "preparation": "Place eggs into a saucepan in a single layer and cover with water by 1 inch. Cover the saucepan and bring to a boil. Remove from the heat and let eggs stand in hot water for 15 minutes. Drain, then run under cold water to cool. Peel and chop eggs; place into a large bowl. Add buttermilk, beets, cucumber, dill, and chives; stir gently to combine. Chill in the refrigerator for 24 hours before serving."
+    },
+    {
+        "id": 3,
+        "category": "soups",
+        "name": "Beef Soup (Kharcho)",
+        "description": "Beef Soup Kharcho (pronounced harcho), is a hearty beef soup in tomato broth with a wonderful blend of spices and plenty of fresh herbs.",
+        "ingridients": [
+            "1.3 kg beef stew meat",
+            "3 table spoon salt (I used fine sea salt)",
+            "4 table spoon light olive oil (or any cooking oil)",
+            "1 medium onion, chopped",
+            "2 celery ribs, finely chopped",
+            "4 cups beef broth + 4 cup filtered water",
+            "700 g. can diced tomatoes",
+            "2 dry bay leaves",
+            "1 table spoon ground coriander seeds",
+            "1 table spoon smoked paprika",
+            "2 garlic cloves, pressed",
+            "1/2 cup uncooked white rice (I used Jasmine)",
+            "2 table spoon fresh lemon juice (from 1 medium lemon)",
+            "1/4 cup fresh parsley, chopped, plus more to serve",
+            "1/4 cup fresh dill, chopped, plus more to serve"
+        ],
+        "preparation": "1. Sprinkle meat with 3 tsp salt and stir. Heat a dutch oven or soup pot over medium/high heat and add 2 Tbsp oil. Once oil is hot, add beef in 2 batches, sautéing until browned on all sides (don’t worry about cooking it through). Remove from pot and set aside. 2. Add chopped onion and celery with more oil as needed. Saute 5 minutes until softened. 3. Add canned tomatoes with their juice, 4 cups broth, 4 cups water, 2 bay leaves and browned beef. Bring to a boil then reduce heat, cover and simmer 1 1/2 to 2 hours or until beef is tender. 4. Add 1/2 cup white rice, 2 Tbsp lemon juice, 2 pressed garlic cloves, 1 tsp coriander, 1 tsp paprika. Cover and simmer 20 minutes or until rice is tender. 5. Remove from heat and add 1/4 cup parsley and 1/4 cup dill then cover and let sit off heat another 10 minutes before serving. Season to taste with salt and pepper if needed and serve with more fresh herbs as desired."
+    },
+    {
+        "id": 4,
+        "category": "Main Corses",
+        "name": "Lithuanian Potato-Meat Dumplings (Cepelinai)",
+        "description": "Lithuanian potato-meat dumplings are known as zeppelins or cepelinai because of their shape. These hearty and rich dumplings take some time to make, but they are well worth the effort.",
+        "ingridients": [
+            "1 pound ground pork, or an equal mix of pork, beef, and veal",
+            "1 medium onion, peeled and finely chopped",
+            "1 1/2 teaspoons kosher salt",
+            "1/4 teaspoon freshly ground black pepper",
+            "1 large egg, beaten",
+            "1 tablespoon freshly squeezed lemon juice",
+            "8 large Idaho potatoes, peeled and finely grated, about 5 pounds",
+            "2 large Idaho or Russet potatoes, about 1 1/2 pounds, peeled, boiled, and riced",
+            "1 medium onion, finely grated",
+            "1 1/2 teaspoons kosher salt, more as needed",
+            "1 tablespoon cornstarch",
+            "1/2 pound bacon, diced ",
+            "1 large onion, coarsely chopped",
+            "1 cup sour cream",
+            "Freshly ground black pepper, to taste",
+            "1 to 2 tablespoons milk, as needed"
+        ],
+        "preparation": "1. MAKE THE MEAT FILLING: In a large bowl, mix together ground meat, finely chopped onion, the salt, a few grinds of pepper, and the egg until well combined. Cover and refrigerate until ready to use. 2 MAKE THE DUMPLINGS: Add the lemon juice to the raw grated potatoes so they don't turn brown. Place them in a fine-mesh cheesecloth or cotton dishtowel, and twist over a large bowl to get rid of the excess water. Pour off the water, reserving the potato starch at the bottom of the bowl. Unwrap the cheesecloth and place the raw potatoes in the bowl with the potato starch you reserved from the bottom of the bowl. Add the cooked riced potatoes, grated onion, and the salt, more to taste. Mix well. Put a large stockpot of well-salted water on to boil. To form the zeppelins, take about 1 cup of dumpling mixture and pat it flat in the palm of the hand. Place 1/4 cup or more of meat mixture in the center and, using slightly dampened hands, fold the potato mixture around the meat into a football shape, sealing well. Continue until both mixtures are used. 3 COOK THE DUMPLINGS: Carefully lower dumplings (in batches if necessary to prevent crowding) into salted, boiling water to which 1 tablespoon of cornstarch has been added (to prevent dumplings from falling apart). Make sure water returns to the boil; reduce the heat to low and continue simmering until the filling is cooked through, 20 to 25 minutes. Remove dumplings with a slotted spoon or strainer, drain briefly on a clean dish towel and place on a heated platte. 4 MAKE THE GRAVY: While the dumplings are boiling, make the gravy. In a medium skillet, fry the bacon and chopped onion until tender. Drain and combine with sour cream and black pepper. Thin with 1 to 2 tablespoons milk if necessary.Ladle the gravy over the dumplings or pass the gravy at the table."
+    },
+    {
+        "id": 5,
+        "category": "Main Corses",
+        "name": "Chebureki",
+        "description": "Chebureki are a fried turnover, filled with ground beef and onions. They also taste great with ground lamb. Chebureki are a classic Crimean, Caucasus and Turkic street food snack, popular across Russia and the former Soviet Union.",
+        "ingridients": [
+            "Ground Beef",
+            "2 small onions",
+            "Salt and Pepper",
+            "Water",
+            "Fresh Parsley"
+        ],
+        "preparation": "Prepare the dough by kneading until smooth and elastic. Allow to rest for 1 hour. Combine the meat filling ingredients, and you're ready to start. Pinch of cut a piece of dough, smaller than a tennis ball, bigger than a golf ball. Roll out into a thin circle, 6-8 round. Place a heaping tablespoon over 1/2 of the dough, evenly spread. Leave 1/2 clean border around the edge of the meat. Fold the dough over the meat, forming half a moon. Press and seal the edges. Use a for the press a pattern around the edges of the seal. Heat 1/2 oil into a frying pan until hot. Fry 2 Chebureki meat pies at a time for 3-4 minutes, carefully turning over each side when golden brown. Remove to a paper towel lined plate to soak up the excess oil. Serve immediately."
+    },
+    {
+        "id": 6,
+        "category": "Main Corses",
+        "name": "Spicy Honey Garlic Wings",
+        "description": "Spicy wings! An all-time favourite ingredient combination, which is honey and garlic, is combined with spicy Korean gochujang and salty soy sauce.",
+        "ingridients": [
+            "2 pounds of chicken wings.",
+            "ow-sodium soy sauce",
+            "Vegetable Oil",
+            "Fresh garlic",
+            "Honey sweetens the sauce",
+            "Gochujang sause"
+        ],
+        "preparation": "Preheat your oven to 450 degrees F.  Line a large baking sheet with aluminum foil and set a cooling rack on it. Set that aside. Pat the wings dry with paper towels and transfer them to a mixing bowl with the salt, pepper, and soy sauce. Toss the wings well until coated. Add the cornstarch and toss until all of the wings are well coated. Next, coat the cooling rack with cooking spray and transfer the wings to it. It’s imperative to lay the wings out so that they are not overlapping or even touching for that matter. The space between the wings will encourage faster cooking and more browning. Bake the wings for 40 minutes. Remove them from the oven and use tongs to turn each wing over. Place them back into the oven to bake for 10 more minutes. Once done, remove from oven and set aside. MAKING THE SAUCE: While the wings are baking, measure out the vegetable oil and minced garlic. Get those into a large skillet. Now turn the heat on to medium. Stir and fry the garlic until fragrant. Do not brown the garlic! We are not browning; we are releasing the natural oils and flavour so that the sauce tastes even better. Once the oil starts to sizzle, only cook for 2 minutes, stirring constantly. Next, add the remaining ingredients and whisk until everything is mixed well. Everything goes into that skillet and I whisk everything together until I’m left with a thick, sticky sauce. Add the wings and toss well to coat. Continue to toss over high heat until the sauce thickens even more and the wings are sticky. Plate, and garnish with parsley and sesame seeds. Serve immediately with celery and carrot sticks with your favourite veggie dip."
+    },
+    {
+        "id": 7,
+        "category": "Desserts",
+        "name": "Homemade Vanilla Ice Cream",
+        "description": "I scream, you scream, we all scream for HOMEMADE ice cream! This sweet treat is rich, creamy, packed with vanilla flavor, and insanely easy to make.",
+        "ingridients": [
+            "Heavy cream ",
+            "Whole milk",
+            "Cane sugar",
+            "Vanilla extract",
+            "Salt"
+        ],
+        "preparation": "At least 12 hours before you want to make homemade ice cream, freeze the bowl of your ice cream maker. It needs to be fully chilled in order for the ice cream to firm up. I recommend doing this step anywhere from 2 to 24 hours in advance. If you do it less than 2 hours ahead, the ice cream won’t thicken as nicely when you churn it.Combine the cream, milk, sugar, vanilla, and salt in a medium saucepan over medium-low heat. Warm for 5 minutes, whisking often, until the sugar is fully dissolved and the mixture is warmed through. Transfer it to a heatproof bowl, cover, and chill for 2 hours or overnight."
+    },
+    {
+        "id": 8,
+        "category": "Desserts",
+        "name": "Crispy Cheese Donuts",
+        "description": "Crispy Cheese Donuts are a tasty after-school snack. These cheesy rings are chewy, crispy, and easy to make with cheddar cheese and quinoa.",
+        "ingridients": [
+            "1 cup finely shredded cheddar cheese",
+            " 1/4 cup cooked quinoa",
+            "1 tablespoon flour",
+            "1/4 teaspoon salt",
+            "1/8 teaspoon garlic powder"
+        ],
+        "preparation": "1 Preheat oven to 400˚F. Put all ingredients into a food processor and pulse until crumbly. 2 Press one tablespoon of the mixture into each of the 12 cavities in a mini donut pan. 3 Bake at 400˚F for 8-10 minutes. 4 Store in a sealed container in the fridge."
+    },
+    {
+        "id": 9,
+        "category": "Beverages",
+        "name": "Margarita",
+        "description": "Margaritas are a classic drink we all know and love. It’s definitely the favorite cocktail around here.",
+        "ingridients": [
+            "Tequila",
+            "Orange liqueur",
+            "Lime Juice",
+            "Margarita Salt",
+            "Agave Syrup",
+            "Fresh Fruit"
+        ],
+        "preparation": "3 ingredients, and an easy ratio to remember:(2:1:1) 2 parts Tequila, 1 part lime juice, 1 part Orange liqueur Other ingridients are optional"
+    },
+    {
+        "id": 10,
+        "category": "Beverages",
+        "name": "Strawberry Peach Mojito",
+        "description": "This easy mocktail recipe features zesty lime, fresh mint, Tropicana Strawberry Peach Paradise Drink, and a delightful garnish, making the perfect non-alcoholic beverage!",
+        "ingridients": [
+            "1/2 lime, juiced",
+            "5 fresh mint leaves, plus fresh mint springs, for garnish",
+            "1/4 cup Tropicana® Strawberry Peach Paradise Drink",
+            "Club soda, to top off",
+            "1 fresh strawberry"
+        ],
+        "preparation": "1 Muddle lime juice and mint together in a highball cocktail glass, until fragrant. 2 Fill glass halfway with ice. Stir in strawberry peach drink. Top off with club soda and garnish with mint springs and a strawberry. Serve and enjoy."
+    },
+    {
+        "id": 11,
+        "category": "Beverages",
+        "name": "Mojito",
+        "description": "When life gives you mint leaves, what do you make? This mint mojito recipe!",
+        "ingridients": [
+            "Fresh mint",
+            "White rum",
+            "Fresh lime juice",
+            "Simple syrup",
+            "Club soda or sparkling water",
+            "Ice"
+        ],
+        "preparation": "First, muddle the mint. Place it in the bottom of a cocktail shaker and use a cocktail muddler to press it until it becomes aromatic. When you’re muddling the mint, your goal isn’t to pulverize it or crush it. Instead, just bruise it slightly to help it release its fragrant oils. This should only take a few taps with your muddler! Second, shake! Add the rum, lime juice, simple syrup, and a handful of ice to the cocktail shaker, and shake it vigorously until the ingredients are completely combined and chilled. Then, strain your mojito. Strain the contents of the cocktail shaker over a glass filled with ice. Top your glass with soda water, and garnish it with lime slices and more fresh mint sprigs. Finally, taste and adjust. This step is essential anytime you’re making a cocktail at home. It gives you a chance to customize your drink to your tastes, finding a balance of flavors you really love. If you prefer a tarter mojito, add an extra squeeze of lime juice. If you want it sweeter, stir in a bit more simple syrup. Not strong enough? Well, you know what to do."
+    },
+    {
+        "id": 12,
+        "category": "Beverages",
+        "name": "Classic Mimosa Recipe",
+        "description": "Learn how to make mimosas at home with this simple recipe! These easy 2-ingredient cocktails are guaranteed to be the star of your next brunch.",
+        "ingridients": [
+            "dry sparkling wine",
+            "orange juice"
+        ],
+        "preparation": "I like to use a 50/50 ratio of dry sparkling wine to orange juice in my mimosas, but make sure you taste and adjust your drink to your liking. You may prefer as much as 2 parts sparkling wine to 1 part orange juice, or the opposite if you like a less alcoholic cocktail."
+    },
+    {
+        "id": 13,
+        "category": "Beverages",
+        "name": "Strawberry Pistachio Smoothie",
+        "description": "This variation is inspired by a pistachio-mint smoothie we had at the Ojai Valley Inn. The pistachios add richness, and the peppermint makes this strawberry smoothie extra refreshing.",
+        "ingridients": [
+            "1 cup non-fat plain kefir",
+            "1 heaping cup frozen strawberries",
+            "2 Tbsp shelled pistachios",
+            "1 tsp dried peppermint",
+            "1/2 cup low-fat milk",
+            "dash of cinnamon",
+            "fresh strawberry for garnish"
+        ],
+        "preparation": "Blend ingredients in blender until smooth. Pour into glass and garnish with fresh strawberry."
+    },
+    {
+        "id": 14,
+        "category": "Beverages",
+        "name": "Bloody Mary",
+        "description": "Most famous vodka cocktail ever.",
+        "ingridients": [
+            "vodka",
+            "tomamto juice",
+            "lemmon juice",
+            "Worcestershire sauce",
+            "hot sauce",
+            "black pepper",
+            "celery salt",
+            "celery stick"
+        ],
+        "preparation": "Put a handful of ice into a large cocktail shaker. Pour 125ml tomato juice, 50ml vodka, 1 tbsp lemon juice and 25ml sweet sherry into the shaker then add ¼ tsp Worcestershire sauce, a few drops of hot sauce and a pinch each of salt, black pepper, celery salt and fennel seeds. Shake hard then strain into a tall glass with ice and garnish with a celery stick, lemon wedge and a cherry tomato."
+    }
+]
+
+# https://api.npoint.io/3e890a99abf7eb2ad0ae
